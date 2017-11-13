056c81644e45355b24c2f2a737aa9cd508714b4b
Sebastian <sebastian.r.rb@gmail.com>
2017-11-13 10:46:17 -0500

    Continue building

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -16,8 +16,75 @@ export default class ProjectList extends React.Component {
 		const searchTerm = this.props.searchTerm;
 		let list;
 
-		if (this.props.searchTerm === "") {
-			list = projects.map(project => {
+		// if (this.props.searchTerm === "") {
+		// 	list = projects.map(project => {
+		// 		let id = project.id;
+		// 		return (
+		// 			<li key={id} className="project-list__project">
+		// 				<Link to={`/project/${id}`}>
+		// 					<h3 className="project-list__project-name">
+		// 						{project.name}
+		// 					</h3>
+		// 					<p className="project-list__project-short-description">
+		// 						{project.shortDescription}
+		// 					</p>
+		// 					<div className="project-list__project-image-container">
+		// 						<img src={project.image} alt="project" />
+		// 					</div>
+		// 				</Link>
+		// 			</li>
+		// 		);
+		// 	});
+		// } else {
+		// 	list = projects
+		// 		.filter(project => {
+		// 			const searchThrough = `${project.name} ${project.shortDescription} ${project.description} ${project.technologies}`.toLowerCase();
+		// 			const searchTermArray = searchTerm.split(" ");
+		// 			let match = false;
+		// 			for (var i = 0; i < searchTermArray.length; i++) {
+		// 				if (searchThrough.indexOf(searchTermArray[i].toLowerCase()) >= 0) {
+		// 					match = true;
+		// 					break;
+		// 				}
+		// 			}
+
+		// 			return match;
+		// 		})
+		// 		.map(project => {
+		// 			let id = project.id;
+		// 			return (
+		// 				<li key={id} className="project-list__project">
+		// 					<Link to={`/project/${id}`}>
+		// 						<h3 className="project-list__project-name">
+		// 							{project.name}
+		// 						</h3>
+		// 						<p className="project-list__project-short-description">
+		// 							{project.shortDescription}
+		// 						</p>
+		// 						<div className="project-list__project-image-container">
+		// 							<img src={project.image} alt="project" />
+		// 						</div>
+		// 					</Link>
+		// 				</li>
+		// 			);
+		// 		});
+		// }
+
+		list = projects
+			.filter(project => {
+				const searchThrough = `${project.name} ${project.shortDescription} ${project.description} ${project.technologies}`.toLowerCase();
+				const searchTermArray = searchTerm.split(" ");
+				let match = false;
+				for (var i = 0; i < searchTermArray.length; i++) {
+					if (searchThrough.indexOf(searchTermArray[i].toLowerCase()) >= 0) {
+						match = true;
+						break;
+					}
+				}
+
+				return match;
+			})
+			.map(project => {
 				let id = project.id;
 				return (
 					<li key={id} className="project-list__project">
@@ -35,32 +102,6 @@ export default class ProjectList extends React.Component {
 					</li>
 				);
 			});
-		} else {
-			list = projects
-				.filter(project => {
-					const searchThrough = `${project.name} ${project.shortDescription} ${project.description} ${project.technologies}`.toLowerCase();
-					const searchTermArray = searchTerm.split(" ");
-					let match = false;
-					for (var i = 0; i < searchTermArray.length; i++) {
-						if (searchThrough.indexOf(searchTermArray[i].toLowerCase()) >= 0) {
-							match = true;
-							break;
-						}
-					}
-
-					return match;
-				})
-				.map(project => {
-					let id = project.id;
-					return (
-						<li key={id} className="project-list__project">
-							<Link to={`/project/${id}`}>
-								{project.name}
-							</Link>
-						</li>
-					);
-				});
-		}
 
 		if (list.length <= 0) {
 			list = (
