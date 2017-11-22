d2c36a5191962730bc4bfcd8ee5f5c25e0fbb347
Sebastian <sebastian.r.rb@gmail.com>
2017-11-21 19:47:14 -0500

    Refine styling

--- src/components/PortfolioDescription.js
+++ src/components/PortfolioDescription.js
@@ -8,7 +8,7 @@ const PortfolioDescription = props => {
 	return (
 		<section className="portfolio-description">
 			<p className="portfolio-description__quote">I'm a React-focused front-end developer.</p>
-			<p className="portfolio-description__quote">Below, you can take a look at some of the projects I've built.</p>
+			<p className="portfolio-description__quote">Below, you can take a look at some of my projects.</p>
 			<ul className="portfolio-description__fact-list" />
 		</section>
 	);
