03a5cec345aca9061bae7853f10ca34c723f6944
Sebastian <sebastian.r.rb@gmail.com>
2017-11-21 21:47:36 -0500

    Refine styling and tech icon; add empty tech objects

--- src/data/projects.js
+++ src/data/projects.js
@@ -6,6 +6,11 @@ export const projects = [
 		description:
 			"The portfolio you are currently viewing is built with React and makes calls to an API to fetch data about my projects in JSON format.",
 		technologies: "React.js, ES6, Vanilla Javascript, Ajax, Sass, APIs",
+		technologyObjects: [
+			{},
+			{},
+			{}
+		],
 		demoLink: "https://sebastianrb.github.io/portfolio/",
 		githubLink: "https://github.com/sebastianrb/portfolio-react",
 		image: "https://image.ibb.co/cumr56/Screen_Shot_2017_11_20_at_9_35_25_PM.png"
@@ -16,8 +21,12 @@ export const projects = [
 		shortDescription: "Landing Page for an Initial Coin Offer built with React.js",
 		description:
 			"My employer launched a cryptocurrency called Skratch, and I built the site to teach the public about the project. The site is built with React and utilizes server-side rendering via a static-site generator caled Gatsby.",
-		technologies:
-			"React, Isomorphic/Server-side Rendering, ES6, Vanilla Javascript, Ajax, ES6, Sass, APIs, Gatsby.js",
+		technologies:"React, Isomorphic/Server-side Rendering, ES6, Vanilla Javascript, Ajax, ES6, Sass, APIs, Gatsby.js",
+		technologyObjects: [
+			{},
+			{},
+			{}
+		],
 		demoLink: "https://www.undergroundhiphop.com/skratch/",
 		githubLink: "",
 		image: "https://image.ibb.co/mEOuVw/skratch.png"
@@ -29,6 +38,11 @@ export const projects = [
 		description:
 			"I was a lead developer on the team that built a student profile site for Bov Academy. The site contains profiles for the school's student body and heavily utilizes multimedia and various animation effects.",
 		technologies: "Vanilla Javascript, CSS, Handlebars.js, Grunt",
+		technologyObjects: [
+			{},
+			{},
+			{}
+		],
 		demoLink: "https://students.bovacademy.com/",
 		githubLink: "",
 		image: "https://image.ibb.co/mdC4Vw/bov.png"
@@ -40,6 +54,11 @@ export const projects = [
 		description:
 			"This weather app allows the user to search for a city and then makes calls to two external weather API's to provide weather forecasting and clothing recommendations. The site's content adjusts dynamically in response to new city searches.",
 		technologies: "React.js, Redux, ES6, Vanilla Javascript, Ajax, Sass, APIs",
+		technologyObjects: [
+			{},
+			{},
+			{}
+		],
 		demoLink: "http://sebastian-react-weather-app.surge.sh/",
 		githubLink: "https://github.com/sebastianrb/react-redux-weather-app",
 		image: "https://image.ibb.co/cHj7iG/weather.png"
@@ -51,6 +70,11 @@ export const projects = [
 		description:
 			"This is a photo gallery featuring some of my amateur photography work. The application is built using the Vue.js JavaScript framework.",
 		technologies: "Vue.js, ES6, Sass",
+		technologyObjects: [
+			{},
+			{},
+			{}
+		],
 		demoLink: "https://sebastianrb.github.io/vue-photo-gallery/",
 		githubLink: "https://github.com/sebastianrb/vue-photo-gallery",
 		image: "https://image.ibb.co/gOo03G/gallery.png"
@@ -62,6 +86,11 @@ export const projects = [
 		description:
 			"My developer profile is meant to help visitors get to know me better both professionally and personally. The site contains a short bio as well as pictures and other information about my life and interests. It also includes contact information.",
 		technologies: "Vanilla Javascript, jQuery Sass, Gulp",
+		technologyObjects: [
+			{},
+			{},
+			{}
+		],
 		demoLink: "https://sebastianrb.github.io/developer-profile/",
 		githubLink: "https://github.com/sebastianrb/developer-profile",
 		image: "https://image.ibb.co/cqe7iG/profile.png"
@@ -72,6 +101,11 @@ export const projects = [
 		shortDescription: "A jigsaw puzzle built with pure Javascript",
 		description: "A jigsaw game!",
 		technologies: "Pure JavaScript, CSS",
+		technologyObjects: [
+			{},
+			{},
+			{}
+		],
 		demoLink: "https://sebastianrb.github.io/jigsaw/",
 		githubLink: "https://github.com/sebastianrb/jigsaw",
 		image: "https://image.ibb.co/j3byqw/jigsaw.png"
@@ -83,6 +117,11 @@ export const projects = [
 		description:
 			"This is a fully functional game of Tic Tac Toe. The computer opponent is moderately intelligent; it will win when possible and often try to block the player when the player is nearing victory. In addition, the computer will sometimes move first to keep things fair and interesting.",
 		technologies: "jQuery, Sass",
+		technologyObjects: [
+			{},
+			{},
+			{}
+		],
 		demoLink: "https://sebastianrb.github.io/tic-tac-toe/",
 		githubLink: "https://github.com/sebastianrb/tic-tac-toe",
 		image: "https://image.ibb.co/nhxiAw/tictactoe.png"
@@ -93,6 +132,11 @@ export const projects = [
 		shortDescription: "A random fact generator using Ajax",
 		description: "A random fatc generator!",
 		technologies: "jQuery, Ajax, CSS",
+		technologyObjects: [
+			{},
+			{},
+			{}
+		],
 		demoLink: "https://sebastianrb.github.io/random-facts/",
 		githubLink: "https://github.com/sebastianrb/random-facts",
 		image: "https://image.ibb.co/bRoYOG/random.png"
@@ -103,6 +147,11 @@ export const projects = [
 		shortDescription: "A photo gallery pulling random Unsplash images built with Handlebars.js",
 		description: "Handlebars gallery",
 		technologies: "Javascript, Handlebars.js, CSS",
+		technologyObjects: [
+			{},
+			{},
+			{}
+		],
 		demoLink: "https://sebastianrb.github.io/handlebars-gallery/",
 		githubLink: "https://github.com/sebastianrb/handlebars-gallery",
 		image: "https://image.ibb.co/cnQrAw/handlebars.png"
